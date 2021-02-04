6dc85021c54e975e4df32562e68706af54fabfd7
Ludovic Stephan <ludovic.stephan@ens.fr>
2021-02-04 09:47:04 +0100

    Problem 119

---
+++ 119.jl
@@ -0,0 +1,20 @@
+function get_max(k)
+    i = 1
+    while 9 * (1 + k * log10(i)) >= i
+        i += 1
+    end
+
+    return i
+end
+
+l = []
+for k in 2:9
+    for n in Int(ceil(10^(1/k))):get_max(k)
+        x = n^k
+        if sum(digits(x)) == n
+            append!(l, x)
+        end
+    end
+end
+
+println(sort(l)[30])
