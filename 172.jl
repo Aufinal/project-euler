8c8e5d896ab9c777c189270a47fb03be2b1b6490
Ludovic Stephan <ludovic.stephan@protonmail.com>
2025-01-28 16:44:53 +0100

    finish 172

--- 172.jl
+++ 172.jl
@@ -1,24 +1,29 @@
-const mem = Dict{Tuple{Int,Int,Int},Vector{Vector{Int}}}()
+const mem = Dict{Tuple{Int,Int,Int},Vector{Dict{Int,Int}}}()
 
 partitions(n, n_parts, max_part) =
     get!(mem, (n, n_parts, max_part)) do
         if n_parts == 0 && (n > 0)
             return []
         elseif n == 0
-            return [[]]
+            return [Dict()]
         else
             res = []
             for part in 1:min(n, max_part)
-                subparts = partitions(n - part, n_parts - 1, part)
-                append!(res, ([part, l...] for l in subparts))
+                subparts = deepcopy(partitions(n - part, n_parts - 1, part))
+                foreach(l -> l[part] = get(l, part, 0) + 1, subparts)
+                append!(res, subparts)
             end
             return res
         end
     end
 
-count_partition(n, d, parts) = (factorial(big(n)) ÷ prod(factorial, parts)) * ((factorial(d) ÷ factorial(d - length(parts))))
+function count_partition(d, parts)
+    n = sum(prod, parts)
+    places = factorial(big(n)) ÷ (prod(p -> factorial(p.first)^p.second, parts) * prod(factorial, values(parts)))
+    digits = ((factorial(d) ÷ factorial(d - sum(values(parts)))))
 
-println(partitions(18, 10, 3))
+    return places * digits * (d - 1) ÷ d
+end
 
-count_numbers(n, d, m) = sum(p -> count_partition(n, d, p), partitions(n, d, m))
+count_numbers(n, d, m) = sum(p -> count_partition(d, p), partitions(n, d, m))
 println(count_numbers(18, 10, 3))
